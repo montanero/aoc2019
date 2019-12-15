cd639f19d602200bc6f3627c34bd60a7762b548b
Roland Bergler <roland@bergler.net>
2019-12-15 11:44:00 +0100

    try for day12b, not working

--- src/test/kotlin/de/montanero/aoc2019/day12/TestDay12.kt
+++ src/test/kotlin/de/montanero/aoc2019/day12/TestDay12.kt
@@ -1,47 +1,83 @@
 package de.montanero.aoc2019.day12
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class TestDay12 {
     @Test
     fun testA() {
         val positions = PlanetFileReader.read("/input12.txt")
-        val planets = positions.map { Planet(it, ThreeDee(0, 0, 0)) }
+        var planets = positions.map { Planet(it, ThreeDee(0, 0, 0)) }
 
         for (i in 1..1000) {
-            applyGravity(planets)
-            applyVelocity(planets)
+            planets = takeTurn (planets)
         }
 
-        val energy = planets.map { it.energy() }.sum()
-        assertEquals(42, energy)
+        val energy = energy(planets)
+        assertEquals(12070, energy)
+    }
+
+    private fun takeTurn(planets: List<TestDay12.Planet>): List<TestDay12.Planet> {
+        val p2 = applyGravity(planets)
+        return applyVelocity(p2)
     }
 
     @Test
     fun testExample() {
         val positions = listOf(ThreeDee(-1, 0, 2), ThreeDee(2, -10, -7), ThreeDee(4, -8, 8), ThreeDee(3, 5, -1))
-        val planets = positions.map { Planet(it, ThreeDee(0, 0, 0)) }
+        var planets = positions.map { Planet(it, ThreeDee(0, 0, 0)) }
 
         for (i in 1..10) {
-            applyGravity(planets)
-            applyVelocity(planets)
+            planets = takeTurn (planets)
         }
 
-        val energy = planets.map { it.energy() }.sum()
+        val energy = energy(planets)
         assertEquals(179, energy)
 
 
     }
 
+    private fun energy(planets: List<Planet>) =
+            planets.map { it.energy() }.sum()
 
-    private fun applyVelocity(planets: List<Planet>) {
-        planets.forEach {
-            it.pos = it.pos + it.vel
+    @Disabled
+    @Test
+    fun testB():Unit {
+        val positions = PlanetFileReader.read("/input12.txt")
+        val initials = positions.map { Planet(it, ThreeDee(0, 0, 0)) }
+        var planets = initials
+        val historicPlanets = mutableMapOf(energy(planets) to 0)
+
+
+        for (i in 1..1_000_000_000) {
+            planets = takeTurn (planets)
+            val e = energy(planets)
+            if (historicPlanets.contains(e) && planets  == turns(initials, historicPlanets[e]!!)) {
+                println(i)
+                System.exit(1)
+            }
+            historicPlanets[e] = i
+        }
+       val x:Int?  = fail ("not found")
+    }
+
+    private fun turns(initials: List<Planet>, turns: Int): List<Planet> {
+        var planets = initials
+        for (i in 1..turns) {
+            planets = takeTurn(planets)
+        }
+        return planets
+    }
+
+    private fun applyVelocity(planets: List<Planet>):List<Planet> {
+        return planets.map {
+            Planet(it.pos + it.vel, it.vel)
         }
     }
 
-    private fun applyGravity(planets: List<Planet>) {
+    private fun applyGravity(planets: List<Planet>): List<Planet> {
         val newVel = planets.map { it.vel }.toMutableList()
         for (i in 0..planets.size - 2) {
             val pi = planets[i]
@@ -52,12 +88,12 @@ class TestDay12 {
                 newVel[j] = newVel[j] - (idiff)
             }
         }
-        planets.zip(newVel).forEach { pair: Pair<Planet, ThreeDee> ->
-            pair.first.vel = pair.second
+        return planets.zip(newVel).map { pair: Pair<Planet, ThreeDee> ->
+            Planet(pair.first.pos,  pair.second)
         }
     }
 
-    data class Planet(var pos: ThreeDee, var vel: ThreeDee) {
+    data class Planet(val pos: ThreeDee, val vel: ThreeDee) {
         fun energy() = potentialEnergy() * kineticEnergy()
         private fun potentialEnergy(): Int = pos.absSum()
         private fun kineticEnergy(): Int = vel.absSum()
